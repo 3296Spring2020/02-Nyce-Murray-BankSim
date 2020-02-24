4c91b8a818d145402a9beab8b1b52ab5ee223d6a
Phil Nyce <tug81999@temple.edu>
2020-02-24 17:21:06 -0500

    made almost no progress, just transferring to my desktop

--- Account.c
+++ Account.c
@@ -1,54 +1,44 @@
 #include "Account.h"
 
-Account *Account_new(int id, int initialBalance) {
+Account *Account_new(int id, int initialBalance)
+{
     Account *a = (Account *)malloc(sizeof(Account));
     a->id = id;
     a->balance = initialBalance;
-    //a->accountlock = NULL;
+    // a->accountlock = NULL;
     pthread_mutex_t newlock = PTHREAD_MUTEX_INITIALIZER;
     a->accountlock = newlock;
     pthread_cond_t newcond = PTHREAD_COND_INITIALIZER;
     a->lowfunds = newcond;
-    a->fundsneeded = -1;
+    a->fundsneeded = 0;
     return a;
 }
 
-void Account_destroy(Account *a) {
-    free(a);
-}
+void Account_destroy(Account *a) { free(a); }
 
-void Account_deposit(Account *a, int amount) {
+void Account_deposit(Account *a, int amount)
+{
     pthread_mutex_lock(&a->accountlock);
     int newBalance = a->balance + amount;
     a->balance = newBalance;
-    if((a->fundsneeded > 0) && (a->balance >= a->fundsneeded)) {
-        a->fundsneeded = -1;
+    if ((a->balance >= a->fundsneeded))
+    {
         pthread_cond_signal(&a->lowfunds);
     }
     pthread_mutex_unlock(&a->accountlock);
 }
 
-int Account_withdraw(Account *a, int amount) {
-    pthread_mutex_lock(&a->accountlock);
-    if(amount <= a->balance) {
+int Account_withdraw(Account *a, int amount)
+{
+    if (a->balance >= amount)
+    {
         int newBalance = a->balance - amount;
         a->balance = newBalance;
-        pthread_mutex_unlock(&a->accountlock);
-        return 1;
-    } else {
-        //wait/notify condition here
-        a->fundsneeded = amount;
-        while(a->fundsneeded < a->balance && a->fundsneeded > 0) pthread_cond_wait(&a->lowfunds, &a->accountlock);
-        if(a->balance >= a->fundsneeded) {
-            int newBalance = a->balance - amount;
-            a->balance = newBalance;
-        }else {
-            pthread_mutex_unlock(&a->accountlock);
-            return 0;
-        }
-        //TEST
-        //if(a->balance < 0) printf("NEGATIVE BALANCE\n");
-        pthread_mutex_unlock(&a->accountlock);
+        a->fundsneeded = 0;
         return 1;
     }
+    a->fundsneeded = amount;
+    return 0;
+    // TEST
+    // if(a->balance < 0) printf("NEGATIVE BALANCE\n");
 }
--- Bank.c
+++ Bank.c
@@ -1,67 +1,83 @@
 #include "Bank.h"
 
-Bank *Bank_new(int numAccounts, int initialBalance) {
+Bank *Bank_new(int numAccounts, int initialBalance)
+{
     Bank *b = (Bank *)malloc(sizeof(Bank));
     b->initialBalance = initialBalance;
     b->numAccounts = numAccounts;
     b->ntransacts = 0;
     b->accounts = (Account **)malloc(numAccounts * sizeof(Account *));
-    for(int i = 0; i < numAccounts; ++i) {
+    for (int i = 0; i < numAccounts; ++i)
+    {
         b->accounts[i] = Account_new(i, initialBalance);
     }
     pthread_mutex_t lk = PTHREAD_MUTEX_INITIALIZER;
     b->bankLock = lk;
-    
+
     return b;
 }
 
-void Bank_destroy(Bank *b) {
-    for(int i = 0; i < b->numAccounts; ++i) {
+void Bank_destroy(Bank *b)
+{
+    for (int i = 0; i < b->numAccounts; ++i)
+    {
         Account_destroy(b->accounts[i]);
     }
     free(b);
 }
 
-void Bank_open(Bank *b) {
-    for(int i = 0; i < b->numAccounts; ++i) {
+void Bank_open(Bank *b)
+{
+    for (int i = 0; i < b->numAccounts; ++i)
+    {
         TransferThreadParameters *params = TransferThreadParameters_new(b, i, b->initialBalance);
         pthread_create(&b->accounts[i]->thread, NULL, transfer_thread, params);
     }
 
-    for(int i = 0; i < b->numAccounts; ++i) {
+    for (int i = 0; i < b->numAccounts; ++i)
+    {
         pthread_join(b->accounts[i]->thread, NULL);
     }
-    
+
     Bank_test(b);
 
     puts("Bank is finished running.");
 }
 
-void Bank_transfer(Bank *b, int from, int to, int amount) {
+void Bank_transfer(Bank *b, int from, int to, int amount)
+{
     // Uncomment line when race condition in Bank_test() has been resolved.
-    if (Bank_shouldTest(b)) Bank_createTester(b);
+    if (Bank_shouldTest(b))
+        Bank_createTester(b);
 
     pthread_mutex_lock(&b->bankLock);
-    if(Account_withdraw(b->accounts[from], amount)) {
-        Account_deposit(b->accounts[to], amount);
-    }
+
+    pthread_mutex_lock(&b->accounts[from]->accountlock);
+    while (!Account_withdraw(b->accounts[from], amount))
+        pthread_cond_wait(&b->accounts[from]->lowfunds, &b->accounts[from]->accountlock);
+    pthread_mutex_unlock(&b->accounts[from]->accountlock);
+    printf("Withdraw from acc %d.\n", from);
+    Account_deposit(b->accounts[to], amount);
+    printf("Withdraw from acc %d.\n", to);
+
     pthread_mutex_unlock(&b->bankLock);
-
-
 }
 
-void Bank_createTester(Bank *b){
-    pthread_t *bankTester = (pthread_t*)malloc(sizeof(pthread_t));
+void Bank_createTester(Bank *b)
+{
+    pthread_t *bankTester = (pthread_t *)malloc(sizeof(pthread_t));
     pthread_create(bankTester, NULL, Bank_test, b);
 }
 
-void *Bank_test(void *bank) {
-    Bank *b = (Bank*) bank;
+void *Bank_test(void *bank)
+{
+    Bank *b = (Bank *)bank;
     int sum = 0;
 
     // critical section
     pthread_mutex_lock(&b->bankLock);
-    for(int i = 0; i < b->numAccounts; ++i) {
+    for (int i = 0; i < b->numAccounts; ++i)
+    {
         pthread_t currentThreadId = pthread_self();
         Account *currAccount = b->accounts[i];
         printf("%lu Account[%d] balance %d\n", pthread_self(), currAccount->id, currAccount->balance);
@@ -70,15 +86,19 @@ void *Bank_test(void *bank) {
     pthread_mutex_unlock(&b->bankLock);
     // end section
     printf("%lu Sum: %d\n", pthread_self(), sum);
-    if(sum != b->numAccounts * b->initialBalance) {
+    if (sum != b->numAccounts * b->initialBalance)
+    {
         printf("%lu Money was gained or lost!\n", pthread_self());
         exit(0);
-    } else {
+    }
+    else
+    {
         printf("%lu The bank is in balance!\n", pthread_self());
     }
 }
 
-int Bank_shouldTest(Bank *b) {
-b->ntransacts++;
-return b->ntransacts % NTEST == 0;
+int Bank_shouldTest(Bank *b)
+{
+    b->ntransacts++;
+    return b->ntransacts % NTEST == 0;
 }
